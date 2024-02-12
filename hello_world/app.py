69d28dbb09ca43e0397114322adc7da429bfdb2e
harishnreddy <harishnreddy@gmail.com>
2024-02-12 14:59:46 -0500

    Update app.py
    po

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "simple change hello world",
+            "message": "123 simple change hello world",
             # "location": ip.text.replace("\n", "")
         }),
     }
